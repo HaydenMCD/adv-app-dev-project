c11ab7f9c8d395d0caeab97b0b96e13dd0317110
Hayden-McD <mcdohr2@student.op.ac.nz>
2022-06-21 17:53:50 +1200

    Fixed auth on each page

--- app-dev-project/src/Components/CreateGameComponent.js
+++ app-dev-project/src/Components/CreateGameComponent.js
@@ -1,58 +1,52 @@
 import React, { useRef } from "react";
 import { Form, Button, Card, Alert } from "react-bootstrap";
 import { collection, addDoc } from "firebase/firestore";
-import { db } from "../firebase"
-import { getAuth } from 'firebase/auth';
-import { useHistory } from 'react-router-dom';
+import { db } from "../firebase";
+import { useHistory } from "react-router-dom";
 
-const CreateGameComponent = () => {
-  const nameRef = useRef();
-  const passwordRef = useRef();
-  const auth = getAuth();
-  const user = auth.currentUser
-  const history = useHistory();
+const CreateGameComponent = ({ authError, isLoggedIn, user, auth }) => {
+    const nameRef = useRef();
+    const passwordRef = useRef();
+    const history = useHistory();
 
-  async function handleSubmit(e) {
-    e.preventDefault()
-    try {
-      const docRef = await addDoc(collection(db, "Games"), {
-        gameName: nameRef.current.value,
-        password: passwordRef.current.value,
-        createdBy: user.displayName,
-        joinable: true,
-        gameOver: false,
-        players: [user.displayName]
-      });
-      console.log("Document written with ID: ", docRef.id);
-    } catch (e) {
-      console.error("Error adding document: ", e);
+    async function handleSubmit(e) {
+        e.preventDefault();
+        try {
+            const docRef = await addDoc(collection(db, 'Games'), {
+                gameName: nameRef.current.value,
+                password: passwordRef.current.value,
+                createdBy: user.displayName,
+                joinable: true,
+                gameOver: false,
+                players: [user.displayName],
+            });
+            console.log('Document written with ID: ', docRef.id);
+        } catch (e) {
+            console.error('Error adding document: ', e);
+        }
     }
-    history.replace("/")
-  }
 
-  return (
-    <Card
-      className="d-felx align-items-center justify-content-center"
-      style={{ minHeight: "100vh" }}>
-      <Card.Body>
-        <h2 className="text-center mb-4">Create a game</h2>
-        {/* {error && <Alert variant="danger">{error}</Alert>} */}
-        <Form onSubmit={handleSubmit}>
-          <Form.Group id="game-name">
-            <Form.Label>Game Name:</Form.Label>
-            <Form.Control ref={nameRef} required />
-          </Form.Group>
-          <Form.Group id="password">
-            <Form.Label>Password:</Form.Label>
-            <Form.Control type="password" ref={passwordRef} />
-          </Form.Group>
-          <Button className="w-100" type="submit">
-            Create Game
-          </Button>
-        </Form>
-      </Card.Body>
-    </Card>
-  );
+    return (
+        <Card className='d-felx align-items-center justify-content-center'>
+            {isLoggedIn ? <Card.Body>
+                <h2 className='text-center mb-4'>Create a game</h2>
+                {authError && <Alert variant='danger'>{authError}</Alert>}
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group id='game-name'>
+                        <Form.Label>Game Name:</Form.Label>
+                        <Form.Control ref={nameRef} required />
+                    </Form.Group>
+                    <Form.Group id='password'>
+                        <Form.Label>Password:</Form.Label>
+                        <Form.Control type='password' ref={passwordRef} />
+                    </Form.Group>
+                    <Button className='w-100' type='submit'>
+                        Create Game
+                    </Button>
+                </Form>
+            </Card.Body> : <Card.Body> Please Login </Card.Body>}
+        </Card>
+    );
 };
 
 export default CreateGameComponent;
